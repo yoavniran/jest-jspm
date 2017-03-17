fb26deba4d8d4e0c8d87d0b1074aa366bc01db26
ArmorDarks <arkahor@gmail.com>
2017-03-17 20:05:59 +0200

    fix lint errors

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,44 @@
-"use strict";
+'use strict'
 
-const gulp = require("gulp"),
-    del = require("del"),
-    gulpBabel = require("gulp-babel"),
-    gulpJest = require("gulp-jest").default,
-    gulpCoveralls = require("gulp-coveralls"),
+const gulp = require('gulp')
+const del = require('del')
+const gulpBabel = require('gulp-babel')
+const gulpJest = require('gulp-jest').default
+const gulpCoveralls = require('gulp-coveralls')
 
-gulp.task("clean:lib", () => del(["lib/**/*"]));
-gulp.task("clean:testOutput", () => del(["test/output/**/*"]));
+gulp.task('clean:lib', () => del(['lib/**/*']))
+gulp.task('clean:testOutput', () => del(['test/output/**/*']))
 
-gulp.task("build", ["clean:lib"], () =>
-    gulp.src("src/**/*.js")
-        .pipe(gulpBabel())
-        .pipe(gulp.dest("lib")));
+gulp.task('build', ['clean:lib'], () =>
+  gulp.src('src/**/*.js')
+    .pipe(gulpBabel())
+    .pipe(gulp.dest('lib')))
 
 const runTest = (withCover) =>
-    gulp.src("")
-        .pipe(gulpJest({
-            "verbose": true,
-            "debug": false,
-            "config": {
-                "roots": ["./test"],
-                "collectCoverage": withCover,
-                "coverageDirectory": "test/output",
-                "collectCoverageFrom": ["src/**/*.js"],
-                "coverageThreshold": {
-                    "global": {
-                        "branches": 90,
-                        "functions": 90,
-                        "lines": 90,
-                        "statements": 90
-                    }
-                }
-            }
-        }));
+  gulp.src('')
+    .pipe(gulpJest({
+      'verbose': true,
+      'debug': false,
+      'config': {
+        'roots': ['./test'],
+        'collectCoverage': withCover,
+        'coverageDirectory': 'test/output',
+        'collectCoverageFrom': ['src/**/*.js'],
+        'coverageThreshold': {
+          'global': {
+            'branches': 90,
+            'functions': 90,
+            'lines': 90,
+            'statements': 90
+          }
+        }
+      }
+    }))
 
-gulp.task("coveralls", ["test:cover"], () =>
-    gulp.src("test/output/**/lcov.info")
-        .pipe(gulpCoveralls()));
+gulp.task('coveralls', ['test:cover'], () =>
+  gulp.src('test/output/**/lcov.info')
+    .pipe(gulpCoveralls()))
 
-gulp.task("test", () => runTest());
-gulp.task("test:cover", ["clean:testOutput"], () => runTest(true));
-gulp.task("default", ["coveralls", "build"]);
+gulp.task('test', () => runTest())
+gulp.task('test:cover', ['clean:testOutput'], () => runTest(true))
+gulp.task('default', ['coveralls', 'build'])
